aec1bbb73815a92577b06fa95bed1bfce9f73aa8
System Administrator <root@Samuels-MacBook-Pro.local>
2016-11-14 17:41:27 -0800

    Automated reinstallation push

--- murraylab_tools/biotek/__init__.py
+++ murraylab_tools/biotek/__init__.py
@@ -1,2 +1,3 @@
 __all__ = ['biotek']
-from .biotek import calibration_data, raw_to_uM, tidy_biotek_data
+from .biotek import calibration_data, raw_to_uM, tidy_biotek_data, \
+                    background_subtract
