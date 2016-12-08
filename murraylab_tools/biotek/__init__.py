2f4bf767dfc02b284b7a2112fa8f5daefd5b1dc7
Sam Clamons <sclamons@gmail.com>
2016-12-07 21:36:37 -0800

    Automated reinstallation push

--- murraylab_tools/biotek/__init__.py
+++ murraylab_tools/biotek/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ['biotek']
 from .biotek import calibration_data, raw_to_uM, tidy_biotek_data, \
-                    background_subtract, endpoint_averages
+                    background_subtract, endpoint_averages, window_averages
