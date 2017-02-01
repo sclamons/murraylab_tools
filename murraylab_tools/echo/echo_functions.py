4a48c1710e728e754423ee361790b1f260b2c074
Sam Clamons <sclamons@gmail.com>
2017-02-01 14:43:10 -0800

    Automated reinstallation push

--- murraylab_tools/echo/echo_functions.py
+++ murraylab_tools/echo/echo_functions.py
@@ -1231,7 +1231,7 @@ class EchoRun():
                                     math.ceil(master_mix.n_extract_aliquots()))
                     text_file.write("\n\tTubes of buffer needed: %d" % \
                                     math.ceil(master_mix.n_buffer_aliquots()))
-                    text_file.write("\n\tMaster Mix (including %.2f /% excess):"\
+                    text_file.write("\n\tMaster Mix (including %.2f %% excess):"\
                                     %(master_mix.mm_excess-1) * 100)
                     for name, vol in master_mix.recipe():
                         text_file.write("\n\t\t%.2f uL %s" % \
