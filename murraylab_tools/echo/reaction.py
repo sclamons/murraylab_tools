8f6410ca2d817c5dc96b4a30e1e2077d39b4b822
Sam Clamons <sclamons@gmail.com>
2017-04-25 17:45:45 -0700

    Automated reinstallation push

--- murraylab_tools/echo/reaction.py
+++ murraylab_tools/echo/reaction.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from echo_source_material import EchoSourceMaterial
-from echo_funtions import *
+from echo_functions import *
 
 __all__ = ["Reaction", "WellReaction", "MasterMix"]
 
