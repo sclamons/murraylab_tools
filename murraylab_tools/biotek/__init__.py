ae4bf14cbe9383a40268c8a55b6bbeaebe49a913
Sam Clamons <sclamons@gmail.com>
2017-05-30 16:52:51 -0700

    Made Biotek scripts compatible with Python 3, hopefully

--- murraylab_tools/biotek/__init__.py
+++ murraylab_tools/biotek/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['biotek']
 from .biotek import calibration_data, raw_to_uM, tidy_biotek_data, \
                     background_subtract, endpoint_averages, window_averages, \
-                    spline_fit, smoothed_derivatives
+                    spline_fit, smoothed_derivatives, read_supplementary_info
