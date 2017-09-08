962c21a6fd26e610044db9b697bfd28d3bb1bb78
Anandh Swaminathan <ananswam@gmail.com>
2017-09-08 16:43:15 -0700

    Made 2 changes. (1.) Updated gitignore to ignore build/ and dist/. (2.) Added an extract_trajectories_only function to make it easier to get back trajectories of time and each measured channel by well.

--- murraylab_tools/biotek/__init__.py
+++ murraylab_tools/biotek/__init__.py
@@ -1,4 +1,5 @@
 __all__ = ['biotek']
 from .biotek import calibration_data, raw_to_uM, tidy_biotek_data, \
                     background_subtract, endpoint_averages, window_averages, \
-                    spline_fit, smoothed_derivatives, read_supplementary_info
+                    spline_fit, smoothed_derivatives, read_supplementary_info, \
+                    extract_trajectories_only
