3a112bc5dc436b853ab73ff43854080314a43f9a
Sam Clamons <sclamons@gmail.com>
2017-10-02 17:18:25 -0700

    Minor or otherwise undocumented automated reinstallation push

--- murraylab_tools/echo/echo_functions.py
+++ murraylab_tools/echo/echo_functions.py
@@ -18,17 +18,38 @@ max_volume  = 65000 # Maximum Echoable volume in an Echo source plate, in nL
 usable_volume = max_volume - dead_volume # Maximum pipettable volume per well
                                          # in an Echo source plate, in nL
 
+# Encoding nonsense (for detecting UTF-8 vs. UTF-16)
+from codecs import BOM_UTF8, BOM_UTF16_BE, BOM_UTF16_LE, \
+                   BOM_UTF32_BE, BOM_UTF32_LE
+
+BOMS = (
+    (BOM_UTF8, "UTF-8"),
+    (BOM_UTF32_BE, "UTF-32-BE"),
+    (BOM_UTF32_LE, "UTF-32-LE"),
+    (BOM_UTF16_BE, "UTF-16-BE"),
+    (BOM_UTF16_LE, "UTF-16-LE"),
+)
+
+def check_bom(data):
+    return [encoding for bom, encoding in BOMS if data.startswith(bom)]
+
+
 def mt_open(filename, setting_code):
-    print("Trying to open a file!")
-    try:
-        return_file = io.open(filename, setting_code, encoding = 'utf-16')
-    except (UnicodeDecodeError, UnicodeError):
-        try:
-            return_file = io.open(filename, setting_code, encoding = 'utf-8')
-        except (UnicodeDecodeError, UnicodeError):
-            raise ValueError(("File %s is neither UTF-8 nor UTF-16. " + \
-                              "Could not open.") % filename)
-    # return_file = io.open(filename, setting_code, encoding = 'utf-16')
+    if setting_code.startswith('w'):
+        return open(filename, setting_code)
+
+    with open(filename, 'r'):
+        supported_encodings = check(next(filename))
+    # try:
+    #     return_file = io.open(filename, setting_code, encoding = 'utf-16')
+    # except (UnicodeDecodeError, UnicodeError):
+    #     try:
+    #         return_file = io.open(filename, setting_code, encoding = 'utf-8')
+    #     except (UnicodeDecodeError, UnicodeError):
+    #         raise ValueError(("File %s is neither UTF-8 nor UTF-16. " + \
+    #                           "Could not open.") % filename)
+    return_file = io.open(filename, setting_code,
+                          encoding = supported_encodings[0])
     return return_file
 
 
