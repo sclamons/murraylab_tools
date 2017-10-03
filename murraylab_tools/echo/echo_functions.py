4fe07291fd0fe03b110f61d5f5a9980f81ed3191
Sam Clamons <sclamons@gmail.com>
2017-10-03 16:26:30 -0700

    Minor or otherwise undocumented automated reinstallation push

--- murraylab_tools/echo/echo_functions.py
+++ murraylab_tools/echo/echo_functions.py
@@ -45,7 +45,7 @@ def mt_open(filename, setting_code):
         supported_encodings = check_bom(next(infile))
         print("File " + filename + " supports the following encodings: " + str(supported_encodings))
     if len(supported_encodings) == 0:
-        encoding = "UTF-16"
+        encoding = "UTF-8"
     else:
         encoding = supported_encodings[0]
     # try:
