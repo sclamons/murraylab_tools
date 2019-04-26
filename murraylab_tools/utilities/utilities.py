d87b0662d5f951d7a313e3dcc70a9fbd02e35e98
sclamons <sclamons@gmail.com>
2019-04-25 19:53:10 -0700

    Adds utilities package with alert functionality.

---
+++ murraylab_tools/utilities/utilities.py
@@ -0,0 +1,92 @@
+import smtplib
+from email.message import EmailMessage
+import warnings
+import __main__ as main
+import os
+import traceback
+
+# We're going to use some GLOBAL VARIABLES to keep track of user information.
+# This should be set once somewhere at the beginning of the program
+# using the set_credentials method below.
+_username = None
+_password = None
+_destination = None
+
+def set_credentials(username, password):
+    if not isinstance(username, str):
+        raise TypeException("Username must be a string.")
+    if not isinstance(password, str):
+        raise TypeException("Password must be a string.")
+    global _username
+    global _password
+    _username = username
+    _password = password
+
+def set_destination(destination):
+    if not isinstance(destination, str):
+        raise TypeException("Destination must be a string.")
+    global _destination
+    _destination = destination
+
+class notify_when_done(object):
+    def __init__(self, done_string, debug_connection = False,
+                  server = "smtp.google.com", port = 587):
+        self.done_string = done_string
+        self.debug_connection = debug_connection
+        self.server = server
+        self.port   = port
+
+        if hasattr(main, "__file__"):
+            self.file = main.__file__
+        else:
+            self.file = "Jupter notebook at " + os.path.abspath("")
+
+    def __enter__(self):
+        # Test connection and credentials.
+        self.login_good = True
+        print("_username: " + _username)
+        print("_password: " + _password)
+        try:
+            server = self.connect_to_server()
+        except Exception as e:
+            warnings.warn("Connection to email server failed. "\
+                          "Run will continue without notifications.",
+                          RuntimeWarning)
+            self.login_good = False
+            if self.debug_connection:
+                print("Connection error is:")
+                print(traceback.format_exc())
+
+    def __exit__(self, type, value, traceback):
+        # Don't bother connecting again if it didn't work the first time.
+        if not self.login_good:
+            return
+
+        # Send an email/text informing the owner of the script what happened.
+        server = self.connect_to_server()
+
+        message = self.file + " reports "
+        if type == None:
+            # Block completed without error.
+            message += "completion of: " + self.done_string
+        else:
+            # Block hit an error.
+            message += "the following error: \n" + traceback.format_exc()
+
+        msg_email = EmailMessage()
+        msg_email.set_content(message)
+        msg_email["Subject"] = ("Error " if type != None else "Success ") +\
+                                "reported by " + self.file
+        msg_email["From"]    = _username
+        msg_email["To"]      = _destination
+
+        server.send_message(msg_email)
+
+    def connect_to_server(self):
+        print("self.server: " + str(self.server))
+        print("self.port: " + str(self.port))
+        server = smtplib.SMTP(self.server, self.port)
+        server.starttls()
+
+        server.login(_username, _password)
+        return server
